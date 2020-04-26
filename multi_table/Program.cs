ee2da7675b91f5bcaf01a1a1a20e583b985a43ba
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 14:18:58 +0200

    Add files via upload

---
+++ multi_table/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace multi_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multi;
+            int sum, sum1, sum2, sum3, sum4, sum5, sum6, sum7, sum8, sum9, sum10;
+            Console.WriteLine("Enter an number to see its multi table: ");
+            multi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You have enterd: " + multi);
+
+            sum = multi * 0;
+            Console.WriteLine(multi + " * 0 = " + sum);
+
+            sum1 = multi * 1;
+            Console.WriteLine(multi + " * 1 = " + sum1);
+
+            sum2 = multi * 2;
+            Console.WriteLine(multi + " * 2 = " + sum2);
+
+            sum3 = multi * 3;
+            Console.WriteLine(multi + " * 3 = " + sum3);
+
+            sum4 = multi * 4;
+            Console.WriteLine(multi + " * 4 = " + sum4);
+
+            sum5 = multi * 5;
+            Console.WriteLine(multi + " * 5 = " + sum5);
+
+            sum6 = multi * 6;
+            Console.WriteLine(multi + " * 6 = " + sum6);
+
+            sum7 = multi * 7;
+            Console.WriteLine(multi + " * 7 = " + sum7);
+
+            sum8 = multi * 8;
+            Console.WriteLine(multi + " * 8 = " + sum8);
+
+            sum9 = multi * 9;
+            Console.WriteLine(multi + " * 9 = " + sum9);
+
+            sum10 = multi * 10;
+            Console.WriteLine(multi + " * 10 = " + sum10);
+
+
+
+
+        }
+    }
+}
